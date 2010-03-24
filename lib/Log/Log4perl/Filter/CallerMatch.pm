c0d39012d6f0c78570b0f797d2945c7853315073
Patrick Donelan <pat@patspam.com>
2010-03-24 14:48:36 -0400

    Switched to Dist::Zilla

--- lib/Log/Log4perl/Filter/CallerMatch.pm
+++ lib/Log/Log4perl/Filter/CallerMatch.pm
@@ -1,77 +1,14 @@
 package Log::Log4perl::Filter::CallerMatch;
 
-use 5.006;
+# ABSTRACT:  Filter Log4perl messages based on call frames
 
+use 5.006;
 use strict;
 use Log::Log4perl::Config;
-use base "Log::Log4perl::Filter";
+use base 'Log::Log4perl::Filter';
 use Carp;
 
-=head1 NAME
-
-Log::Log4perl::Filter::CallerMatch - Filter log messages based on call frames
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-=head1 DESCRIPTION
-
-This Log4perl custom filter checks the call stack using caller() and filters
-the subroutine and package using user-provided regular expressions. You can specify
-a specific call frame to test against, or have the filter iterate through a range of call frames.
-
-=head1 SYNOPSIS
-
- log4perl.logger = ALL, A1
- log4perl.appender.A1        = Log::Log4perl::Appender::TestBuffer
- log4perl.appender.A1.Filter = MyFilter
- log4perl.appender.A1.layout = Log::Log4perl::Layout::SimpleLayout
-    
- log4perl.filter.MyFilter                = Log::Log4perl::Filter::CallerMatch
- log4perl.filter.MyFilter.SubToMatch     = WebGUI::Session::ErrorHandler
- log4perl.filter.MyFilter.PackageToMatch = Flux::
- log4perl.filter.MyFilter.StringToMatch  = Operand1
- 
-=head1 CONFIGURATION OPTIONS
-
-You can use the following options:
-
-=head2 StringToMatch
-
-A perl5 regular expression, matched against the log message.
-
-=head2 AcceptOnMatch
-
-Defines if the filter is supposed to pass or block the message on a match (C<true> or C<false>).
-
-=head2 PackageToMatch
-
-A perl5 regular expression, matched against the 1st item in the array returned by caller() (e.g. "package")
-
-=head2 SubToMatch
-
-A perl5 regular expression, matched against the 4th item in the array returned by caller() (e.g. "subroutine")
-
-=head2 CallFrame
-
-The call frame to use when requesting information from caller(). (e.g. $i in caller($i)
-
-=head2 MinCallFrame
-
-The first call frame tested against when iterating through a series of call frames. Ignored if CallFrame specified.
-
-=head2 MaxCallFrame
-
-The last call frame tested against when iterating through a series of call frames. Ignored if CallFrame specified.
-
-=head1 FUNCTIONS
-
-=head2 new
+=method new
 
 Constructor. Refer to L<Log::Log4perl::Filter> for more information
 
@@ -101,7 +38,7 @@ sub new {
     return $self;
 }
 
-=head2 ok
+=method ok
 
 Decides whether log message should be accepted or not. Refer to L<Log::Log4perl::Filter> for more information
 
@@ -124,44 +61,54 @@ sub ok {
     return !$self->{AcceptOnMatch};
 }
 
-=head1 AUTHOR
+1;
+__END__
 
-Patrick Donelan, C<< <pat at patspam.com> >>
+=head1 DESCRIPTION
 
-=head1 BUGS
+This Log4perl custom filter checks the call stack using caller() and filters
+the subroutine and package using user-provided regular expressions. You can specify
+a specific call frame to test against, or have the filter iterate through a range of call frames.
 
-Please report any bugs or feature requests to C<bug-log-log4perl-filter-callermatch at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Log-Log4perl-Filter-CallerMatch>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+=head1 SYNOPSIS
 
-=head1 SUPPORT
+ log4perl.logger = ALL, A1
+ log4perl.appender.A1        = Log::Log4perl::Appender::TestBuffer
+ log4perl.appender.A1.Filter = MyFilter
+ log4perl.appender.A1.layout = Log::Log4perl::Layout::SimpleLayout
+    
+ log4perl.filter.MyFilter                = Log::Log4perl::Filter::CallerMatch
+ log4perl.filter.MyFilter.SubToMatch     = WebGUI::Session::ErrorHandler
+ log4perl.filter.MyFilter.PackageToMatch = Flux::
+ log4perl.filter.MyFilter.StringToMatch  = Operand1
+ 
+=attr StringToMatch
 
-You can find documentation for this module with the perldoc command.
+A perl5 regular expression, matched against the log message.
 
-    perldoc Log::Log4perl::Filter::CallerMatch
+=attr AcceptOnMatch
 
+Defines if the filter is supposed to pass or block the message on a match (C<true> or C<false>).
 
-You can also look for information at:
+=attr PackageToMatch
 
-=over 4
+A perl5 regular expression, matched against the 1st item in the array returned by caller() (e.g. "package")
 
-=item * RT: CPAN's request tracker
+=attr SubToMatch
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Log4perl-Filter-CallerMatch>
+A perl5 regular expression, matched against the 4th item in the array returned by caller() (e.g. "subroutine")
 
-=item * AnnoCPAN: Annotated CPAN documentation
+=attr CallFrame
 
-L<http://annocpan.org/dist/Log-Log4perl-Filter-CallerMatch>
+The call frame to use when requesting information from caller(). (e.g. $i in caller($i)
 
-=item * CPAN Ratings
+=attr MinCallFrame
 
-L<http://cpanratings.perl.org/d/Log-Log4perl-Filter-CallerMatch>
+The first call frame tested against when iterating through a series of call frames. Ignored if CallFrame specified.
 
-=item * Search CPAN
+=attr MaxCallFrame
 
-L<http://search.cpan.org/dist/Log-Log4perl-Filter-CallerMatch>
-
-=back
+The last call frame tested against when iterating through a series of call frames. Ignored if CallFrame specified.
 
 =head1 SEE ALSO
 
@@ -171,13 +118,6 @@ L<Log::Log4perl::Filter::LevelMatch>,
 L<Log::Log4perl::Filter::LevelRange>,
 L<Log::Log4perl::Filter::Boolean>
 
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Patrick Donelan, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
 =cut
 
-1;    # End of Log::Log4perl::Filter::CallerMatch
+1;
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-	use_ok( 'Log::Log4perl::Filter::CallerMatch' );
-}
-
-diag( "Testing Log::Log4perl::Filter::CallerMatch $Log::Log4perl::Filter::CallerMatch::VERSION, Perl $], $^X" );
